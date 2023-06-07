5693597f2f12977c4306178ed5b26fe6a5556ebe
julietadirzieh <julidirzieh@gmail.com>
2023-06-07 11:39:15 -0300

    fix: dependencies

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,15 +1,29 @@
-export default {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "standard",
+    "eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-unused-vars": "warn",
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
